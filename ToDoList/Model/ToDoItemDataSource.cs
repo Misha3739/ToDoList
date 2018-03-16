d6aa4124b947fed3302e6c147f0c1e9c5a6f5ee9
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-16 03:47:31 -0700

    Sort feature implemented

--- ToDoList/Model/ToDoItemDataSource.cs
+++ ToDoList/Model/ToDoItemDataSource.cs
@@ -1,22 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using AppKit;
+using Foundation;
 
 namespace ToDoList.Model
 {
     public class ToDoItemDataSource : NSTableViewDataSource
     {
-        private readonly IList<ToDoItem> _items = new List<ToDoItem>();
+        private readonly List<ToDoItem> _items = new List<ToDoItem>();
 
-        public IList<ToDoItem> Items => _items;
+        public List<ToDoItem> Items => _items;
 
         public ToDoItemDataSource()
         {
         }
 
-		public override nint GetRowCount(NSTableView tableView)
-		{
-            return _items.Count;
-		}
-	}
+        public override nint GetRowCount(NSTableView tableView)
+        {
+            return Items.Count;
+        }
+
+
+        public void Sort(string key, bool ascending)
+        {
+
+            // Take action based on key
+            switch (key)
+            {
+                case "Name":
+                    if (ascending)
+                    {
+                        Items.Sort((x, y) => x.Name.CompareTo(y.Name));
+                    }
+                    else
+                    {
+                        Items.Sort((x, y) => -1 * x.Name.CompareTo(y.Name));
+                    }
+                    break;
+                case "Important":
+                    if (ascending)
+                    {
+                        Items.Sort((x, y) => x.IsImportant.CompareTo(y.IsImportant));
+                    }
+                    else
+                    {
+                        Items.Sort((x, y) => -1 * x.IsImportant.CompareTo(y.IsImportant));
+                    }
+                    break;
+            }
+
+        }
+
+        public override void SortDescriptorsChanged(NSTableView tableView, NSSortDescriptor[] oldDescriptors)
+        {
+            // Sort the data
+            if (oldDescriptors.Length > 0)
+            {
+                // Update sort
+                Sort(oldDescriptors[0].Key, oldDescriptors[0].Ascending);
+            }
+            else
+            {
+                // Grab current descriptors and update sort
+                NSSortDescriptor[] tbSort = tableView.SortDescriptors;
+                Sort(tbSort[0].Key, tbSort[0].Ascending);
+            }
+
+            // Refresh table
+            tableView.ReloadData();
+        }
+    }
 }
