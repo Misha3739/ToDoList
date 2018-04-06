9d0b32149a174e6a4c696dd9a1638171471dc5f1
Mikhail.udot <Mikhail.udot@venafi.com>
2018-04-06 19:57:45 +0300

    * ToDoList.sln: * TestClass.cs: * packages.config: * AssemblyInfo.cs:
    * ToDoList.Tests.csproj: NUnit test project added

---
+++ ToDoList.Tests/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ToDoList.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
